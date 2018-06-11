a388841ae23cbd4d57b856e3ed2de32db3b04f04
Karthrand <735867550@qq.com>
2018-06-11 16:53:26 +0800

    增加文件存储页

---
+++ app/src/main/java/com/oude/Storage/FileActivity.java
@@ -0,0 +1,14 @@
+package com.oude.Storage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FileActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.file);
+    }
+}
--- app/src/main/java/com/oude/Storage/MainActivity.java
+++ app/src/main/java/com/oude/Storage/MainActivity.java
@@ -1,14 +1,39 @@
 package com.oude.Storage;
 
-import android.app.*;
 import android.os.*;
-
+import android.app.Activity;
+import java.util.List;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import java.util.ArrayList;
 public class MainActivity extends Activity 
 {
+    private List<MyList> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initMylist();
+        RecyclerView recycleView = (RecyclerView) findViewById(R.id.mainRecyclerView1);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recycleView.setLayoutManager(layoutManager);
+        MyListAdapter adapter =new MyListAdapter(list);
+        recycleView.setAdapter(adapter);
     }
+
+
+    private void initMylist()
+    {
+            MyList  list1 = new MyList("文件存储",R.drawable.ic_launcher);
+            list.add(list1);
+            MyList  list2 = new MyList("List2",R.drawable.ic_launcher);
+            list.add(list2);
+            MyList  list3 = new MyList("List3",R.drawable.ic_launcher);
+            list.add(list3);
+            MyList  list4 = new MyList("List4",R.drawable.ic_launcher);            list.add(list4);
+   
+        
+    }
+
 }
